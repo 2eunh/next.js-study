c01f7a8ba05451b883b7cf6a0e312864fc3e1fcf
unknown <eeunh2@gmail.com>
2024-12-23 17:09:11 +0900

    SEO 설정

--- onebite-next/section02/src/pages/search/index.tsx
+++ onebite-next/section02/src/pages/search/index.tsx
@@ -2,24 +2,13 @@ import BookItem from "@/components/book-item";
 import SearchableLayout from "@/components/searchable-layout";
 import fetchBooks from "@/lib/fetch-books";
 import { BookData } from "@/types";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { ReactNode, useEffect, useState } from "react";
 
 
 
-// export const getStaticProps = async (context : GetStaticProps) => {
-//   const q = context.query.q;
-//   const books = await fetchBooks(q as string);
-
-//   return{
-//     props : {
-//       books
-//     }
-//   }
-// }
-
 export default function Page(
-  // {books} : InferGetServerSidePropsType<typeof getServerSideProps>
 ){
   
   const [books, setBooks] = useState<BookData[]>([]);
@@ -41,6 +30,12 @@ export default function Page(
 
   return (
     <div>
+      <Head>
+        <title>한입북스 - 검색결과</title>
+        <meta property="og:image" content="/thumbnail.png"/>
+        <meta property="og:title" content="한입북스 - 검색결과"/>
+        <meta property="og:description" content="한입 북스에 등록된 도서들을 만나보세요"/>
+      </Head>
       {books.map((book) => (
         <BookItem key={book.id} {...book}/>
       ))}
